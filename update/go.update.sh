8967b157d2b72db794ab88871b25b6a4ea053168
taki.036731 <taki.036731@gmail.com>
2024-02-21 14:10:27 +0000

    フォルダ構成変更

--- update/go.update.sh
+++ update/go.update.sh
@@ -5,6 +5,7 @@ BASE_NAME=result/history.csv
 sufix=`tail -1 ${BASE_NAME} | cut -d , -f 1`
 BACK_NAME=result/history-${sufix}.csv
 
+mkdir -p logs
 mv ${BASE_NAME} ${BACK_NAME}
 python update_history.py ${BACK_NAME} ${BASE_NAME}
-python parse.py ${BASE_NAME} 2>&1 > ${sufix}.log
+python parse.py ${BASE_NAME} > logs/${sufix}.log 2>&1
