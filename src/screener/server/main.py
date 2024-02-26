daf028eecafec8bf4c6950326936a41bc616acfe
taki.036731 <taki.036731@gmail.com>
2024-02-26 14:09:56 +0000

    add logger

--- src/screener/server/main.py
+++ src/screener/server/main.py
@@ -1,7 +1,19 @@
+import os
 from fastapi import FastAPI
+import logging
+import json
+import logging.config
+
+print(os.path.dirname(__file__))
+with open(os.path.join(os.path.dirname(__file__), 'logging.json'), 'r', encoding='utf-8') as f:
+    j = json.load(f)
+    logging.config.dictConfig(j)
+    print(j)
+
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return { "message": "Hello World" }
+    logging.getLogger(__name__).debug('Hello world')
+    return { "message": "Hello World!!" }
--- src/screener/server/repository/securities.py
+++ src/screener/server/repository/securities.py
@@ -1,22 +1,26 @@
 import sys
 import pandas as pd
 import sqlite3 as sql
+import logging
 from collections.abc import Iterator
 import settings
 from screener.server.models.security import Security
 
 class Securities:
+    __logger: logging.Logger = logging.getLogger(__name__)
     @staticmethod
     def table() -> str:
         return __class__.__name__
 
     def __init__(self) -> None:
+        self.__logger.info(f'Open Database "{settings.DB_PATH}"')
         conn = sql.connect(settings.DB_PATH)
         self.__itmes = []
         cur = conn.cursor()
         cur.execute(f'SELECT "コード", "銘柄名", "市場・商品区分", "33業種区分", "17業種区分", "規模区分" FROM {Securities.table()}')
         self.__itmes = [Security(c[0], c[1], c[2], c[3], c[4], c[5]) for c in cur.fetchall()]
         conn.close()
+        self.__logger.debug(f'get {len(self.__itmes)} items.')
 
     def codes(self, market:str = 'プライム（内国株式）', add_t: bool = True) -> Iterator[str]:
         suffix = '.T' if add_t else ''
