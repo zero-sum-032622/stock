5cbdb172be3664ea8cb35682711a72f5324e037a
zero-sum-032622 <your.yak.032622@gmail.com>
2024-03-31 06:55:49 +0000

    version up

--- src/screener/server/update.py
+++ src/screener/server/update.py
@@ -3,7 +3,6 @@ from screener.server.repository.history import History
 from screener.server.repository.securities import Securities
 
 if __name__ == "__main__":
-    pass
     h = History()
     s = Securities()
-    h.update(s.codes(None), sys.argv[1], sys.argv[1])
+    h.update(s.codes(None), sys.argv[1], sys.argv[2])
