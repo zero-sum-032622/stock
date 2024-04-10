8004261b8be40ab3430d5ed144f45bc3285fa015
zero-sum-032622 <your.yak.032622@gmail.com>
2024-04-11 01:05:03 +0900

    version up

--- src/screener/models/analyzer.py
+++ src/screener/models/analyzer.py
@@ -27,5 +27,64 @@ class Analyzer:
         self.df[col + 'u2'], self.df[col + 'mid'], self.df[col + 'd2'] = ta.BBANDS(self.df.Close, period, 2, 2, 0)
         self.df[col + 'u3'], self.df[col + 'mid'], self.df[col + 'd3'] = ta.BBANDS(self.df.Close, period, 3, 3, 0)
         return self
-    
-    
+
+    def add_zigzag(self, cp: float) -> Analyzer:
+        self.__calculate_zigzag(cp)
+        self.__calculate_profit()
+        return self
+
+    def __calculate_profit(self) -> None:
+        expected = []
+        period = []
+        for row in self.df.itertuples():
+            profit = self.df.at[row.switch, 'Close'] - row.Close
+            expected.append(profit / row.Close)
+            period.append(self.df.index.get_loc(row.switch) - self.df.index.get_loc(row.Index))
+        self.df['expected'] = expected
+        self.df['period'] = period
+
+
+    def __calculate_zigzag(self, cp: float) -> None:
+        prev_trend = 1
+        prev_provisional = self.df.Close.iloc[0]
+
+        trend = []
+        provisional = []
+
+        for c in self.df.Close:
+            change = (c - prev_provisional) / prev_provisional
+            cur_trend = 1 if change > cp \
+                else -1 if change < -cp \
+                else prev_trend
+            cur_provisional = abs(max(cur_trend * c, cur_trend * prev_provisional))
+            trend.append(cur_trend)
+            provisional.append(cur_provisional)
+            prev_trend = cur_trend
+            prev_provisional = cur_provisional
+
+        self.df['trend'] = trend
+        self.df['provisional'] = provisional
+
+        fixed = []
+        switch = []
+        zigzag = []
+        cur_fixed = 0
+        prev_fixed = provisional[-1]
+        prev_switch = self.df.index[-1]
+        for r in self.df.iloc[::-1].itertuples():
+            cur_fixed = abs(max(r.trend * r.provisional, r.trend * prev_fixed))
+            fixed.append(cur_fixed)
+            prev_fixed = cur_fixed
+            switch.append(prev_switch)
+            if r.Close == cur_fixed:
+                prev_switch = r.Index
+                zigzag.append(cur_fixed)
+            else:
+                zigzag.append(None)
+
+        switch.reverse()
+        zigzag.reverse()
+        self.df.drop(columns=['trend', 'provisional'])
+        self.df['zigzag'] = zigzag
+        self.df['zigzag'].interpolate(inplace=True)
+        self.df['switch'] = switch
--- src/screener/update.py
+++ src/screener/update.py
@@ -1,8 +1,20 @@
 import sys
 from screener.repository.history import History
 from screener.repository.securities import Securities
+import datetime
 
 if __name__ == "__main__":
     h = History()
     s = Securities()
-    h.update(s.codes(None), sys.argv[1], sys.argv[2])
+    if len(sys.argv) == 3:
+        begin: datetime.date = datetime.datetime.strptime(sys.argv[1], '%Y-%m-%d').date()
+        end: datetime.date = datetime.datetime.strptime(sys.argv[2], '%Y-%m-%d').date()
+    elif len(sys.argv) == 2:
+        begin = datetime.date = datetime.datetime.strptime(sys.argv[1], '%Y-%m-%d').date()
+        end = datetime.date.today()
+    else:
+        begin = h.latest()
+        end = datetime.date.today()
+
+    print(f'get data : {begin} - {end}')
+    h.update(s.codes(None), begin, end)
